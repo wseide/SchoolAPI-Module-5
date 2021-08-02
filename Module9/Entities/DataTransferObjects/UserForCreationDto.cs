b1c09345c67173a088b3b74e5a14710abf017384
Wismy <53704939+wseide@users.noreply.github.com>
2021-08-02 15:41:00 -0400

    Fixing things

---
+++ Module9/Entities/DataTransferObjects/UserForCreationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities.DataTransferObjects
+{
+    public class UserForCreationDto
+    {
+        [Required(ErrorMessage = "User name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+        [MinLength(5, ErrorMessage = "Minimum length for the Name is 5 characters.")]
+        public string UserName { get; set; }
+
+    }
+}
